7e987fe3ac1c8a3061181c48eb3b9ea6408545e1
Torbjørn <litiumzzz@gmail.com>
2018-06-07 03:23:47 +0200

    gps icon

---
+++ src/components/Content.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export class Content extends Component {
+  render() {
+    return (
+        <main>
+          <h1>Sunshine</h1>
+          <hr />
+          <section className="content">
+            <article className="post1">
+              <h2>Sunshine</h2>
+              <p>Instant weather where you are!</p>
+            </article>
+            <article className="post2">
+              <h2>Yay</h2>
+              <p>
+                sint tamen dolore culpa noster ipsum velit multos nulla nisi
+              </p>
+            </article>
+            <article className="post3">
+              <h2>Title</h2>
+              <p>quid anim aliqua ipsum quis magna velit irure tempor nisi</p>
+            </article>
+          </section>
+        </main>
+    )
+  }
+}
+
+export default Content;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,34 +1,14 @@
 import React, { Component } from "react";
 import WeatherDisplay from "./WeatherDisplay";
+import Content from './Content';
 
-class Layout extends Component {
+export class Layout extends Component {
   render() {
     return (
       <div className="container">
+
         <WeatherDisplay />
-
-
-        <main>
-          <h1>Sunshine</h1>
-          <hr />
-          <div className="content">
-            <article className="post1">
-              <h2>Simple weather app</h2>
-              <p></p>
-            </article>
-            <article className="post2">
-              <h2>Title</h2>
-              <p>
-                sint tamen dolore culpa noster ipsum velit multos nulla nisi
-              </p>
-            </article>
-            <article className="post3">
-              <h2>Title</h2>
-              <p>quid anim aliqua ipsum quis magna velit irure tempor nisi</p>
-            </article>
-          </div>
-        </main>
-
+        <Content />
 
       </div>
     );
