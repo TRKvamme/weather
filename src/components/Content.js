5ae2083adc1359b559969de540c1b784057f1717
Torbjørn <21195798+TRKvamme@users.noreply.github.com>
2019-03-10 19:35:15 +0100

    Update Content.js
    text

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,18 +8,18 @@ export class Content extends Component {
           <h2>Wherever you are</h2>
           <section className="content">
             <article className="post1">
-              <h2>Where you are</h2>
-              <p>Instant weather where you are!</p>
+              <h2>One day</h2>
+              <p>I will put some content here.</p>
             </article>
             <article className="post2">
-              <h2>Anywhere</h2>
+              <h2>And it will</h2>
               <p>
-                Or anywhere else in the world. 
+                Be great!
               </p>
             </article>
             <article className="post3">
-              <h2>Title</h2>
-              <p>quid anim aliqua ipsum quis magna velit irure tempor nisi</p>
+              <h2>But for now</h2>
+              <p>There is nothing here.</p>
             </article>
           </section>
         </main>
